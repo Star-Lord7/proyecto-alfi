eac99c9056cb93182c11b18363ec65aa452b2d3e
Star-Lord7 <samuelnasa375@gmail.com>
2025-12-12 23:52:33 -0600

    Revert "Primera prueba usando el modelo de IA Generativa"
    This reverts commit cf228c2b5450ae895b554206efb6f199c9078a09.

--- backend/src/controllers/finanzasController.js
+++
@@ -1,8 +0,0 @@
-import * as finanzasModel from '../models/finanzasModel.js';
-
-const finanzas = async (req, res) => {
-  const mensaje = await finanzasModel.finanzas();
-  res.send(mensaje);
-}
-
-export { finanzas };
--- backend/src/routes/finanzasRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as finanzasController from '../controllers/finanzasController.js';
-
-const router = express.Router();
-
-router.get('/finanzas', finanzasController.finanzas);
-
-export default router;
