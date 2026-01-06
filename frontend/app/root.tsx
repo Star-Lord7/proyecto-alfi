3d96f5e6c7c83064a72ee5bb730aa1e1b2a6d1eb
Star-Lord7 <samuelnasa375@gmail.com>
2026-01-06 12:24:56 -0600

    Conexión con el backend

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home";
 import Finanzas from "./routes/finanzas";
+import Preguntas from "./routes/Preguntas";
 
 export default function Root() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/finanzas" element={<Finanzas />} />
+      <Route path="/preguntas" element={<Preguntas />} />
     </Routes>
   );
 }
