2bd74c25ac99ed7e9f848c59c329c89d6dc84f59
Star-Lord7 <samuelnasa375@gmail.com>
2026-01-06 13:29:28 -0600

    Conexión con el backend

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div style={{ padding: 40 }}>
+    <div style={{ padding: 40, textAlign: "center", flexDirection: "column", display: "flex", gap: 20 }}>
       <h1>Home</h1>
       <a href="/finanzas">Ir a Finanzas</a>
+      <a href="/preguntas">Preguntas IA</a>
     </div>
   );
 }
-
