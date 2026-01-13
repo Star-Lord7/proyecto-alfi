efaeb0487ecdbf3104c53097dcda0ab3be26d912
Star-Lord7 <samuelnasa375@gmail.com>
2026-01-13 15:46:43 -0600

    Primer prototipo del Frontend llamando a los Temas desde la API

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -9,11 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return (
-    <div style={{ padding: 40, textAlign: "center", flexDirection: "column", display: "flex", gap: 20 }}>
-      <h1>Home</h1>
-      <a href="/finanzas">Ir a Finanzas</a>
-      <a href="/preguntas">Preguntas IA</a>
-    </div>
-  );
+  return <Welcome />;
 }
